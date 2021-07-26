d3ab93fb0a0630111c7c29609c0a1a8692a89b6a
Andrew Sharrem <asharrem@gmail.com>
2021-07-26 17:28:50 +1200

    grub HDMI enable

--- freeze_fix.sh
+++ freeze_fix.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # disable xdisplay
 # systemctl set-default multi-user.target
-# sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet\ splash\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/g" /etc/default/grub
+# Remove quiet splash from GRUB
+sudo sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet\ splash\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/g" /etc/default/grub
+echo -e "\n Removed quiet splash from GRUB ... \n"
 if [ ! -e /etc/default/grub.d ]; then
   echo -e "\n Making grub.d ... \n"
   sudo mkdir /etc/default/grub.d
 fi
 sudo rm /etc/default/grub.d/*nxos*.cfg
-# video=HDMI-A-1:1920x1080@60:D
-sudo tee /etc/default/grub.d/50_nxos.cfg > /dev/null << EOF
-GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX nomodeset i915.modeset=0 intel_idle.max_cstate=1"
+sudo tee /etc/default/grub.d/50_nxos_cstate.cfg > /dev/null << EOF
+GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX intel_idle.max_cstate=1"
+EOF
+sudo tee /etc/default/grub.d/60_nxos_hdmi.cfg > /dev/null << EOF
+GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX video=HDMI-A-1:1920x1080@60:D"
 EOF
 sudo update-grub
 echo -e "\n *** Reboot Required *** \n"
