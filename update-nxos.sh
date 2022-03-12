0afae0cb146642b4c0538b32c8c3a3151015e0ef
Andrew Sharrem <asharrem@gmail.com>
2022-03-13 09:44:15 +1300

    apt update password loop fixed. Choices if block.

--- update-nxos.sh
+++ update-nxos.sh
@@ -56,8 +56,7 @@ do
   TERM=ansi whiptail --title "$TITLE" --infobox "\n Running apt update..." 22 68
   if ! sudo -S <<< "$SU_PASS" apt -y -qq update; then
     # Ask password if default failed
-    SU_PASS=$(whiptail --title "$TITLE" --passwordbox "\n Please enter password for $USER:" 22 68 3>&1 1>&2 2>&3)
-    if ! $?; then
+    if ! SU_PASS=$(whiptail --title "$TITLE" --passwordbox "\n Please enter password for $USER:" 22 68 3>&1 1>&2 2>&3); then
       # Exit on Cancel
       TERM=ansi whiptail --title "$TITLE" --infobox "\n apt update failed!" 22 68
       sleep 3
@@ -94,230 +93,224 @@ CHOICES=$(whiptail --title "$TITLE" --separate-output --checklist "Choose option
   "12" "Install a specific Nx Witness Client Build" OFF \
   "13" "Install a specific Nx Witness Server Build" OFF 3>&1 1>&2 2>&3)
 
-if [ -z "$CHOICES" ]; then
-  # user hit Cancel or unselected all options
-  clear
-  exit 1
-else
-  for CHOICE in $CHOICES; do
-    case $CHOICE in
-    "01")
-      # Download & Install Workstation PoE Drivers
-      file_name="ds-wseli-poe.deb"
-      if ! download "$WebHostFiles/$file_name"; then
+for CHOICE in $CHOICES; do
+  case $CHOICE in
+  "01")
+    # Download & Install Workstation PoE Drivers
+    file_name="ds-wseli-poe.deb"
+    if ! download "$WebHostFiles/$file_name"; then
+      continue
+    fi
+    if ! install_deb "$file_name"; then
+      continue
+    fi
+  ;;
+  "02")
+    # Update Hostname
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n Updating Hostname to MAC address syntax..." 22 68
+    sleep 0.5
+    # Set Machine Hostname to Last 4 digits of first eth found
+    # reset so we can test for null
+    unset first_eth
+      # shellcheck disable=SC2010
+    first_eth=$(ls /sys/class/net | grep -m1 ^e)
+    if [[ -n "$first_eth" ]]; then
+      # shellcheck disable=SC2002
+      macaddy=$(cat /sys/class/net/"$first_eth"/address | tr -d ':' | grep -o '....$')
+    fi
+    ServerName="${ServerName}-${macaddy}"
+    sudo hostnamectl set-hostname "$ServerName"
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n Hostname = ${ServerName}" 22 68
+    sleep 0.5
+
+    # udpdate hosts file with new ServerName
+    sudo sed -i 's/127.0.1.1	'"${HOSTNAME}"'/127.0.1.1	'"${ServerName}"'/g' /etc/hosts
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n DNS Updated" 22 68
+    sleep 0.5
+  ;;
+  "03")
+    file_name_list="chrome-remote-desktop_current_amd64.deb google-chrome-stable_current_amd64.deb nxwitness-*.deb"
+    for file_name in $file_name_list
+    do
+      TERM=ansi whiptail --title "$TITLE" --infobox "\n Removing $file_name ..." 22 68
+      sleep 0.5
+      rm "$file_name" > /dev/null 2>&1
+    done
+  ;;
+  "04")
+    # Download Chrome files if they don't exist, then install them
+    file_name_list="google-chrome-stable_current_amd64.deb chrome-remote-desktop_current_amd64.deb"
+    for file_name in $file_name_list
+    do
+      if ! download "https://dl.google.com/linux/direct/$file_name"; then
         continue
       fi
       if ! install_deb "$file_name"; then
         continue
       fi
-    ;;
-    "02")
-      # Update Hostname
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n Updating Hostname to MAC address syntax..." 22 68
-      sleep 0.5
-      # Set Machine Hostname to Last 4 digits of first eth found
-      # reset so we can test for null
-      unset first_eth
-        # shellcheck disable=SC2010
-      first_eth=$(ls /sys/class/net | grep -m1 ^e)
-      if [[ -n "$first_eth" ]]; then
-        # shellcheck disable=SC2002
-        macaddy=$(cat /sys/class/net/"$first_eth"/address | tr -d ':' | grep -o '....$')
-      fi
-      ServerName="${ServerName}-${macaddy}"
-      sudo hostnamectl set-hostname "$ServerName"
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n Hostname = ${ServerName}" 22 68
-      sleep 0.5
+    done
+    #Do Post Install actions
+    #
+    # No Longer needed?? - Add user to Chrome Remote Desktop user Group
+    # TERM=ansi whiptail --title "$TITLE" --infobox "\n Adding $USER to Chrome Remote Desktop Group..." 22 68
+    # sleep 0.5
+    # sudo usermod -a -G chrome-remote-desktop "$USER"
 
-      # udpdate hosts file with new ServerName
-      sudo sed -i 's/127.0.1.1	'"${HOSTNAME}"'/127.0.1.1	'"${ServerName}"'/g' /etc/hosts
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n DNS Updated" 22 68
+    # Create Chrome Browser Managed Policy
+    file_name="/etc/opt/chrome/policies/managed/nxos.json"
+    if [ ! -f "$file_name" ]; then
+      TERM=ansi whiptail --title "$TITLE" --infobox "\n Setting Chrome Browser Policy..." 22 68
       sleep 0.5
-    ;;
-    "03")
-      file_name_list="chrome-remote-desktop_current_amd64.deb google-chrome-stable_current_amd64.deb nxwitness-*.deb"
-      for file_name in $file_name_list
-      do
-        TERM=ansi whiptail --title "$TITLE" --infobox "\n Removing $file_name ..." 22 68
-        sleep 0.5
-        rm "$file_name" > /dev/null 2>&1
-      done
-    ;;
-    "04")
-      # Download Chrome files if they don't exist, then install them
-      file_name_list="google-chrome-stable_current_amd64.deb chrome-remote-desktop_current_amd64.deb"
-      for file_name in $file_name_list
-      do
-        if ! download "https://dl.google.com/linux/direct/$file_name"; then
-          continue
-        fi
-        if ! install_deb "$file_name"; then
-          continue
-        fi
-      done
-      #Do Post Install actions
-      #
-      # No Longer needed?? - Add user to Chrome Remote Desktop user Group
-      # TERM=ansi whiptail --title "$TITLE" --infobox "\n Adding $USER to Chrome Remote Desktop Group..." 22 68
-      # sleep 0.5
-      # sudo usermod -a -G chrome-remote-desktop "$USER"
-
-      # Create Chrome Browser Managed Policy
-      file_name="/etc/opt/chrome/policies/managed/nxos.json"
-      if [ ! -f "$file_name" ]; then
-        TERM=ansi whiptail --title "$TITLE" --infobox "\n Setting Chrome Browser Policy..." 22 68
-        sleep 0.5
-        # create file first because tee will not
-        sudo mkdir -p "${file_name%/*}"
-        # use tee to write to file because sudo cat <<EOF is BAD.
-        sudo tee $file_name >/dev/null <<EOF
+      # create file first because tee will not
+      sudo mkdir -p "${file_name%/*}"
+      # use tee to write to file because sudo cat <<EOF is BAD.
+      sudo tee $file_name >/dev/null <<EOF
 {
-  "distribution": {
-    "suppress_first_run_bubble": true,
-    "make_chrome_default": true,
-    "make_chrome_default_for_user": true,
-    "suppress_first_run_default_browser_prompt": true,
-  },
-  "PasswordManagerEnabled": false,
-  "BackgroundModeEnabled": false,
+"distribution": {
+  "suppress_first_run_bubble": true,
+  "make_chrome_default": true,
+  "make_chrome_default_for_user": true,
+  "suppress_first_run_default_browser_prompt": true,
+},
+"PasswordManagerEnabled": false,
+"BackgroundModeEnabled": false,
 }
 EOF
-      fi
-    ;;
-    "05")
-      # Download & Install Nx Client
-      file_name="nxwitness-client-${NxFulVer}-linux64.deb"
-      if ! download "https://updates.networkoptix.com/default/$NxBuild/linux/$file_name"; then
-          continue
-      fi
-      if ! install_deb "$file_name"; then
+    fi
+  ;;
+  "05")
+    # Download & Install Nx Client
+    file_name="nxwitness-client-${NxFulVer}-linux64.deb"
+    if ! download "https://updates.networkoptix.com/default/$NxBuild/linux/$file_name"; then
         continue
-      fi
-    ;;
-    "06")
-      # Download & Install Nx Server
-      file_name="nxwitness-server-${NxFulVer}-linux64.deb"
-      if ! download "https://updates.networkoptix.com/default/$NxBuild/linux/$file_name"; then
-          continue
-      fi
-      if ! install_deb "$file_name"; then
+    fi
+    if ! install_deb "$file_name"; then
+      continue
+    fi
+  ;;
+  "06")
+    # Download & Install Nx Server
+    file_name="nxwitness-server-${NxFulVer}-linux64.deb"
+    if ! download "https://updates.networkoptix.com/default/$NxBuild/linux/$file_name"; then
         continue
-      fi
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n Applying Nx Storage permissions Fix..." 22 68
-      sleep 0.5
-      # Enable Nx AnalyticsDbStoragePermissions
-      if ! curl "http://admin:admin@127.0.0.1:7001/api/systemSettings?forceAnalyticsDbStoragePermissions=true"; then
-        TERM=ansi whiptail --title "$TITLE" --infobox "\n Failed to apply Nx Storage permissions Fix!" 22 68
-        sleep 3
-      fi
-    ;;
-    "07")
-      # Download & Install Cockpit Advanced
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n Installing 45drives sharing scripts..." 22 68
-      # sleep 0.5
-      # Needs GPG to add repos
-      sudo apt -y -qq install gpg
-      # advanced file support by 45drives
-      curl -sSL https://repo.45drives.com/setup | sudo bash
-      sudo apt -y -qq install \
-      crudini \
-      cockpit-file-sharing \
-      cockpit-navigator \
-      gvfs-backends \
-      gvfs-fuse
-      
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n Setting up smb.conf..." 22 68
-      file_name=/etc/samba/smb.conf
-      # remove leading spaces & tabs so crudini does not fail
-      sudo sed -i.bak 's/^[ \t]*//' $file_name
-      # add key pair to samba
-      sudo crudini --set $file_name global include registry
-    ;;
-    "08")
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n Updating Grub..." 22 68
-      sudo sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet\ splash\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/g" /etc/default/grub
-      sudo update-grub
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n Boot Splash turned OFF" 22 68
-      sleep 0.5
-    ;;
-    "09")
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n Applying current freeze fixes..." 22 68
-      sleep 0.5
-      # Create grub.d folder
-      if [ ! -e /etc/default/grub.d ]; then
-        sudo mkdir /etc/default/grub.d
-      fi
-      # Remove any existing NxOS grub settings
-      # Don't care if does not exist
-      sudo rm /etc/default/grub.d/*nxos*.cfg
-      sudo tee /etc/default/grub.d/50_nxos_cstate.cfg > /dev/null << EOF
+    fi
+    if ! install_deb "$file_name"; then
+      continue
+    fi
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n Applying Nx Storage permissions Fix..." 22 68
+    sleep 0.5
+    # Enable Nx AnalyticsDbStoragePermissions
+    if ! curl "http://admin:admin@127.0.0.1:7001/api/systemSettings?forceAnalyticsDbStoragePermissions=true"; then
+      TERM=ansi whiptail --title "$TITLE" --infobox "\n Failed to apply Nx Storage permissions Fix!" 22 68
+      sleep 3
+    fi
+  ;;
+  "07")
+    # Download & Install Cockpit Advanced
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n Installing 45drives sharing scripts..." 22 68
+    # sleep 0.5
+    # Needs GPG to add repos
+    sudo apt -y -qq install gpg
+    # advanced file support by 45drives
+    curl -sSL https://repo.45drives.com/setup | sudo bash
+    sudo apt -y -qq install \
+    crudini \
+    cockpit-file-sharing \
+    cockpit-navigator \
+    gvfs-backends \
+    gvfs-fuse
+    
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n Setting up smb.conf..." 22 68
+    file_name=/etc/samba/smb.conf
+    # remove leading spaces & tabs so crudini does not fail
+    sudo sed -i.bak 's/^[ \t]*//' $file_name
+    # add key pair to samba
+    sudo crudini --set $file_name global include registry
+  ;;
+  "08")
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n Updating Grub..." 22 68
+    sudo sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet\ splash\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/g" /etc/default/grub
+    sudo update-grub
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n Boot Splash turned OFF" 22 68
+    sleep 0.5
+  ;;
+  "09")
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n Applying current freeze fixes..." 22 68
+    sleep 0.5
+    # Create grub.d folder
+    if [ ! -e /etc/default/grub.d ]; then
+      sudo mkdir /etc/default/grub.d
+    fi
+    # Remove any existing NxOS grub settings
+    # Don't care if does not exist
+    sudo rm /etc/default/grub.d/*nxos*.cfg
+    sudo tee /etc/default/grub.d/50_nxos_cstate.cfg > /dev/null << EOF
 GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX i915.enable_rc6=0"
 EOF
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n Updating Grub..." 22 68
-      sudo update-grub
-    ;;
-    "10")
-      # Download nxos-default-settings.deb
-      file_name="nxos-default-settings.deb"
-      if ! download "$WebHostFiles/$file_name"; then
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n Updating Grub..." 22 68
+    sudo update-grub
+  ;;
+  "10")
+    # Download nxos-default-settings.deb
+    file_name="nxos-default-settings.deb"
+    if ! download "$WebHostFiles/$file_name"; then
+      continue
+    fi
+    if ! install_deb "$file_name"; then
+      continue
+    fi
+    # remove live cd autologin
+    sudo rm /etc/lightdm/lightdm.conf
+  ;;
+  "11")
+    # Uninstall Nx Server & Client
+    file_name_list="networkoptix-mediaserver networkoptix-client"
+    for file_name in $file_name_list
+    do
+      if ! sudo dpkg -r "$file_name"; then
         continue
       fi
-      if ! install_deb "$file_name"; then
+    done
+  ;;
+  "12")
+    # Download & Install Specific Nx Client
+    NxMajVer=$(TERM=ansi whiptail --title "$TITLE" --inputbox "\n Install Nx Witness Client\nEnter Nx Major Version eg. 4.2.0" 22 68 3>&1 1>&2 2>&3)      
+    NxBuild=$(TERM=ansi whiptail --title "$TITLE" --inputbox "\n Enter Nx Build Number eg. 32840" 22 68 3>&1 1>&2 2>&3)
+    NxFulVer="$NxMajVer.$NxBuild"
+    file_name="nxwitness-client-${NxFulVer}-linux64.deb"
+    if ! download "https://updates.networkoptix.com/default/$NxBuild/linux/$file_name"; then
         continue
-      fi
-      # remove live cd autologin
-      sudo rm /etc/lightdm/lightdm.conf
-    ;;
-    "11")
-      # Uninstall Nx Server & Client
-      file_name_list="networkoptix-mediaserver networkoptix-client"
-      for file_name in $file_name_list
-      do
-        if ! sudo dpkg -r "$file_name"; then
-          continue
-        fi
-      done
-    ;;
-    "12")
-      # Download & Install Specific Nx Client
-      NxMajVer=$(TERM=ansi whiptail --title "$TITLE" --inputbox "\n Install Nx Witness Client\nEnter Nx Major Version eg. 4.2.0" 22 68 3>&1 1>&2 2>&3)      
-      NxBuild=$(TERM=ansi whiptail --title "$TITLE" --inputbox "\n Enter Nx Build Number eg. 32840" 22 68 3>&1 1>&2 2>&3)
-      NxFulVer="$NxMajVer.$NxBuild"
-      file_name="nxwitness-client-${NxFulVer}-linux64.deb"
-      if ! download "https://updates.networkoptix.com/default/$NxBuild/linux/$file_name"; then
-          continue
-      fi
-      if ! install_deb "$file_name"; then
+    fi
+    if ! install_deb "$file_name"; then
+      continue
+    fi
+  ;;
+  "13")
+    # Download & Install Specific Nx Server
+    NxMajVer=$(TERM=ansi whiptail --title "$TITLE" --inputbox "\n Install Nx Witness Server\nEnter Nx Major Version eg. 4.2.0" 22 68 3>&1 1>&2 2>&3)      
+    NxBuild=$(TERM=ansi whiptail --title "$TITLE" --inputbox "\n Enter Nx Build Number eg. 32840" 22 68 3>&1 1>&2 2>&3)
+    NxFulVer="$NxMajVer.$NxBuild"
+    file_name="nxwitness-server-${NxFulVer}-linux64.deb"
+    if ! download "https://updates.networkoptix.com/default/$NxBuild/linux/$file_name"; then
         continue
-      fi
-    ;;
-    "13")
-      # Download & Install Specific Nx Server
-      NxMajVer=$(TERM=ansi whiptail --title "$TITLE" --inputbox "\n Install Nx Witness Server\nEnter Nx Major Version eg. 4.2.0" 22 68 3>&1 1>&2 2>&3)      
-      NxBuild=$(TERM=ansi whiptail --title "$TITLE" --inputbox "\n Enter Nx Build Number eg. 32840" 22 68 3>&1 1>&2 2>&3)
-      NxFulVer="$NxMajVer.$NxBuild"
-      file_name="nxwitness-server-${NxFulVer}-linux64.deb"
-      if ! download "https://updates.networkoptix.com/default/$NxBuild/linux/$file_name"; then
-          continue
-      fi
-      if ! install_deb "$file_name"; then
-        continue
-      fi
-      TERM=ansi whiptail --title "$TITLE" --infobox "\n Applying Nx Storage permissions Fix..." 22 68
-      sleep 0.5
-      # Enable Nx AnalyticsDbStoragePermissions
-      if ! curl "http://admin:admin@127.0.0.1:7001/api/systemSettings?forceAnalyticsDbStoragePermissions=true"; then
-        TERM=ansi whiptail --title "$TITLE" --infobox "\n Failed to apply Nx Storage permissions Fix!" 22 68
-        sleep 3
-      fi
-    ;;
-    *)
-      echo "Unsupported item $CHOICE!" >&2
-      exit 1
-    ;;
-    esac
-  done
-fi
+    fi
+    if ! install_deb "$file_name"; then
+      continue
+    fi
+    TERM=ansi whiptail --title "$TITLE" --infobox "\n Applying Nx Storage permissions Fix..." 22 68
+    sleep 0.5
+    # Enable Nx AnalyticsDbStoragePermissions
+    if ! curl "http://admin:admin@127.0.0.1:7001/api/systemSettings?forceAnalyticsDbStoragePermissions=true"; then
+      TERM=ansi whiptail --title "$TITLE" --infobox "\n Failed to apply Nx Storage permissions Fix!" 22 68
+      sleep 3
+    fi
+  ;;
+  *)
+    echo "Unsupported item $CHOICE!" >&2
+    break
+  ;;
+  esac
+done
 TERM=ansi whiptail --title "$TITLE" --infobox "\n Applying System Updates..." 22 68
 sleep 0.5
 sudo apt -y -qq upgrade
